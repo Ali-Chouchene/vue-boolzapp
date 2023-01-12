c5c65605839c2b867f5033c6a54468d7bf76a7fb
Ali-Chouchene <choucheneali9801@gmail.com>
2023-01-12 12:50:49 +0100

    added files and dissected the base

---
+++ script/script.js
@@ -0,0 +1,19 @@
+console.log("VUE OK", Vue);
+
+const app = Vue.createApp({
+    name: "Vue-Carousel",
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+
+});
+
+app.mount("#root");
