1ba816200c6fa195d42a4f42070af9bce1e39dc4
Ali-Chouchene <choucheneali9801@gmail.com>
2023-01-13 16:55:20 +0100

    fixed the chat

--- script/data_milestone2.js
+++ script/data_milestone2.js
@@ -8,11 +8,7 @@ const contacts = [
       text: 'Hai portato a spasso il cane?',
       status: 'sent'
     },
-    {
-      date: '10/01/2020 15:50:00',
-      text: 'Ricordati di dargli da mangiare',
-      status: 'sent'
-    },
+
     {
       date: '10/01/2020 16:15:22',
       text: 'Tutto fatto!',
--- script/script.js
+++ script/script.js
@@ -1,5 +1,10 @@
 // console.log("VUE OK", Vue);
 
+//* LUNOX*//
+const DateTime = luxon.DateTime;
+
+let now = "";
+
 const app = Vue.createApp({
     name: "Boolzapp",
     data() {
@@ -7,24 +12,66 @@ const app = Vue.createApp({
             data,
             contacts,
             currentI: 0,
+            newMessage: "",
+            DateTime,
         }
     },
     computed: {
-
-
+        currentContact() {
+            return this.contacts[this.currentI];
+        },
+        currentChat() {
+            return this.currentContact.messages;
+        },
     },
     methods: {
         getAvatarUrl(avatar) {
             return `img/avatar${avatar}.jpg`;
         },
-        setCurrentI(i) {
-            this.currentI = i;
-        }
+        setCurrentI(index) {
+            this.currentI = index;
+        },
+        getCurrentTime() {
+            return DateTime.now().toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS);
+        },
+        addMessage() {
+            if (this.newMessage) {
+                const newMess = {
+                    date: this.getCurrentTime(),
+                    text: this.newMessage,
+                    status: 'sent',
+
+                };
+                this.currentContact.messages.push(newMess);
+                this.resetInput();
+                this.$refs.inputFocus.focus();
+
+
+                setTimeout(this.okPush, 1000);
+            }
+
+        },
+        /**risposta**/
+        okPush() {
+            const newReceived = {
+                date: this.getCurrentTime(),
+                text: "Ok!",
+                status: 'received',
+            };
+            this.currentContact.messages.push(newReceived);
+        },
+        resetInput() {
+            this.newMessage = "";
+        },
+        createReceived() {
+
+        },
+        // pushCreateReceived() {
+        //     setTimeout(this.createReceived, 5000);
+
+        // }
     },
 
 });
-
-
-
 
 app.mount("#root");
